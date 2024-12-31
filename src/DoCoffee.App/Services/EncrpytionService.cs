2ddefa4b23c330255141c8164eb12107ec02d248
Paulo Fernando <paulo.fdo@hotmail.com>
2024-12-31 18:29:56 -0300

    Cria serviço de encriptação

---
+++ src/DoCoffee.App/Services/EncrpytionService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DoCoffee.App.Services
+{
+    public class EncrpytionService
+    {
+        private readonly string _encryptionKey;
+        private readonly byte[] _encryptionKeyInBytes;
+        private static readonly byte[] IV = { 12, 34, 56, 78, 90, 102, 114, 126 };
+        public EncrpytionService(string encryptionKey)
+        {
+            _encryptionKey = encryptionKey ?? throw new ArgumentNullException(nameof(encryptionKey));
+            _encryptionKeyInBytes = Encoding.UTF8.GetBytes(encryptionKey.Substring(0, 8));
+        }
+        public string Encrypt(string value)
+        {
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+
+            using var des = new DESCryptoServiceProvider();
+            using var ms = new MemoryStream();
+            using var cs = new CryptoStream(ms, des.CreateEncryptor(_encryptionKeyInBytes, IV), CryptoStreamMode.Write);
+            var input = Encoding.UTF8.GetBytes(value);
+            cs.Write(input, 0, input.Length);
+            cs.FlushFinalBlock();
+            return Convert.ToBase64String(ms.ToArray());
+        }
+        public string Decrypt(string value)
+        {
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+
+            using var des = new DESCryptoServiceProvider();
+            using var ms = new MemoryStream();
+            var input = Convert.FromBase64String(value.Replace(" ", "+"));
+            using var cs = new CryptoStream(ms, des.CreateDecryptor(_encryptionKeyInBytes, IV), CryptoStreamMode.Write);
+            cs.Write(input, 0, input.Length);
+            cs.FlushFinalBlock();
+            return Encoding.UTF8.GetString(ms.ToArray());
+        }
+    }
+}
