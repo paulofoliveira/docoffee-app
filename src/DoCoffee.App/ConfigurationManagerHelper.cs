f02a389227ab33ebddbb20447d02bd65d0abbbe9
paulofoliveira <paulo.fdo@hotmail.com>
2025-01-04 13:09:00 -0300

    Decrypta com ConfigurationManagerHelper

---
+++ src/DoCoffee.App/ConfigurationManagerHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Configuration;
+
+namespace DoCoffee.App
+{
+    public static class ConfigurationManagerHelper
+    {
+        public static class AppSettings
+        {
+            public static string Get(string key)
+            {
+                if (key == null)
+                    throw new ArgumentNullException(nameof(key));
+
+                var value = ConfigurationManager.AppSettings[key];
+
+                if (EnvironmentHelper.IsDevelopment())
+                    return value;
+
+                return App.EncryptionService.Decrypt(value);
+            }
+        }
+    }
+}
